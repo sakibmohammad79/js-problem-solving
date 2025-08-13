bd2a51d39fdf2c7770dac4ad274d0ef4d8317b58
sakib <mohammadsakib7679@gmail.com>
2025-08-13 16:44:08 +0600

    problem solve check leaf year

---
+++ 7.check_leaf_year.js
@@ -0,0 +1,10 @@
+const checkLeafYear = (year) => {
+    if((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)){
+        console.log(`${year} is a leap year.`);
+    }
+    else{
+        console.log(`${year} is not a leap year.`);
+    }
+}
+
+checkLeafYear(2020);
